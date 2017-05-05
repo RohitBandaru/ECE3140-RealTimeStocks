deaff9a5d51dac707ff399a3104cdf35ce13b3c8
Rohit Bandaru <rb696@cornell.edu>
2017-05-05 16:45:17 -0400

    tried andrew's readme

---
+++ Objects/adafruit_ledbackpack.d
@@ -0,0 +1,1 @@
+.\objects\adafruit_ledbackpack.o: Adafruit_LED_Backpack-master 2\Adafruit_LEDBackpack.cpp
---
+++ RTE/_Target_1/RTE_Components.h
@@ -0,0 +1,50 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'network-t' 
+ * Target:  'Target 1' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "fsl_device_registers.h"
+
+#define RTE_CMSIS_RTOS                  /* CMSIS-RTOS */
+        #define RTE_CMSIS_RTOS_RTX5             /* CMSIS-RTOS Keil RTX5 */
+#define RTE_CMSIS_RTOS2                 /* CMSIS-RTOS2 */
+        #define RTE_CMSIS_RTOS2_RTX5            /* CMSIS-RTOS2 Keil RTX5 */
+        #define RTE_CMSIS_RTOS2_RTX5_SOURCE     /* CMSIS-RTOS2 Keil RTX5 Source */
+#define RTE_DEVICE_CONFIGURATION_FRDM_K64F
+#define RTE_DEVICE_DRIVER_GPIO
+#define RTE_DEVICE_DRIVER_RTC
+#define RTE_DEVICE_HAL_ENET
+#define RTE_DEVICE_HAL_GPIO
+#define RTE_DEVICE_HAL_LPTMR
+#define RTE_DEVICE_HAL_MCG
+#define RTE_DEVICE_HAL_OSC
+#define RTE_DEVICE_HAL_PORT
+#define RTE_DEVICE_HAL_RTC
+#define RTE_DEVICE_HAL_SIM
+#define RTE_DEVICE_HAL_UART
+#define RTE_DEVICE_OSA_BM
+#define RTE_DEVICE_STARTUP_KINETIS      /* Device Startup for Kinetis Series */
+#define RTE_DEVICE_SYSTEM_CLOCK
+#define RTE_DEVICE_SYSTEM_INTERRUPT
+#define RTE_DEVICE_UTILITY_DEBUG_CONSOLE
+#define RTE_DEVICE_UTILITY_MISC
+#define RTE_Drivers_ETH_MAC0            /* Driver ETH_MAC0 */
+#define RTE_Drivers_PHY_KSZ8081RNA      /* Driver PHY KSZ8081RNA/RND */
+#define RTE_Network_Core                /* Network Core */
+          #define RTE_Network_Release             /* Network Release Version */
+#define RTE_Network_DNS_Client          /* Network DNS Client */
+#define RTE_Network_Interface_ETH_0     /* Network Interface ETH 0 */
+#define RTE_Network_Socket_UDP          /* Network Socket UDP */
+
+#endif /* RTE_COMPONENTS_H */
